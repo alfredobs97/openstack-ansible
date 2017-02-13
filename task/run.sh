40195f59f0e9d4f3469d0ecad32449a3a48e1af8
Alfredo Bautista <alfredobautista1@gmail.com>
2017-02-13 18:23:21 +0100

    Delete run.sh

--- task/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Iniciando"
-vagrant up --no-provision
-echo "Provisionar"
-vagrant provision
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory tasks/openstack.yml
-echo "Finalizado"
