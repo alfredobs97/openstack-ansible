ca93a1ef8b377abb175b7ca0e21b78348e66701d
Externoak <dariomr1234@gmail.com>
2017-02-13 19:02:47 +0100

    Fixed writing

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ echo "Iniciando"
 vagrant up --no-provision
 echo "Provisionar"
 vagrant provision
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory tasks/openstack.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory task/openstack.yml
 echo "Finalizado"
