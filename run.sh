71aafd1afd25bc2a39f5fcf458e442c4dc09d31c
alfredobs97 <alfredobautista1@gmail.com>
2017-02-13 18:22:39 +0100

    Cambiando ansible

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Iniciando"
+vagrant up --no-provision
+echo "Provisionar"
+vagrant provision
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory tasks/openstack.yml
+echo "Finalizado"
